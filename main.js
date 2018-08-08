4b0a50aaa75264ca385235c41f9800d444837fab
Jack Deadman <jdeadman1@sheffield.ac.uk>
2018-08-09 00:26:52 +0100

    Improve main drum

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 var Drum = (function() {
 
+  var selectors = {
+    note: '.hang-drum__note'
+  };
+
   function Drum(node) {
     this.node = node;
     arrangeNotes(node);
   }
 
   Drum.prototype.bind = function() {
-    var notes = this.node.querySelectorAll('.hang-drum__note');
+    var notes = this.node.querySelectorAll(selectors.note);
     var that = this;
 
     notes.forEach(function(note) {
@@ -22,22 +26,26 @@ var Drum = (function() {
     audio.play();
   };
 
+  // Private methods
+  // ==============================================
   var toRadians = function(angle) {
     return angle * (Math.PI / 180);
   }
 
   var arrangeNotes = function(node) {
-    var startingAngle = toRadians(parseInt(node.dataset.startingAngle));
-    var notes = node.querySelectorAll('.hang-drum__note');
+    var startingAngle = toRadians(parseInt(node.dataset.startingAngle || 0));
+    var notes = node.querySelectorAll(selectors.note);
     var angleDelta = 2*Math.PI / (notes.length-1);
-    var radius = 110;
+    var radius = node.dataset.radius || 110;
 
     notes.forEach(function(note) {
-      loadAudio(note);
+      loadAudio(note, node);
     });
 
+    // The first note starts in the center
     notes = Array.prototype.slice.call(notes, 1);
 
+    // Parametric equation of a circle to draw notes around the drum
     notes.forEach(function(note, index) {
       var theta = startingAngle + (angleDelta * index);
       var x = Math.round(Math.cos(theta)*radius);
@@ -46,13 +54,26 @@ var Drum = (function() {
     });
   };
 
-  var loadAudio = function(note) {
-    var noteString = note.dataset.note;
+  var loadAudio = function(note, node) {
     var audio = document.createElement('audio');
-    audio.src = 'audio/'+noteString+'.mp3';
+    audio.src = getAudioFileName(note, node);
     audio.className = 'hidden';
     note.appendChild(audio);
-  }
+
+    audio.addEventListener('play', handlers.playHandler);
+    audio.addEventListener('ended', handlers.stopHandler);
+  };
+
+  var getAudioFileName = function(note, node) {
+    var data = node.dataset;
+    var note = note.dataset.note;
+    return data.fileLocation + '/' + note + '.' +(data.extension || 'mp3');
+  };
+
+  var handlers = {
+    playHandler: function(e) { e.target.parentNode.classList.add('active') },
+    stopHandler: function(e) { e.target.parentNode.classList.remove('active') }
+  };
 
   return Drum;
 
@@ -61,15 +82,17 @@ var Drum = (function() {
 
 var drums = (function() {
 
+  var selectors = {
+    drum: '.hang-drum'
+  };
 
-  var drumNodes = document.querySelectorAll('.hang-drum');
+  var drumNodes = document.querySelectorAll(selectors.drum);
   var drums = [];
   drumNodes.forEach(function(drum) {
     drum = new Drum(drum);
     drum.bind();
     drums.push(drum);
   });
-
 
   return drums;
 
